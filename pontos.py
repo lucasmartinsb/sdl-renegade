9b71a61984fa762024b11271b118ac3470336e74
Lucas <lucasmartinsdeb@gmail.com>
2024-09-25 07:36:48 -0300

    [feat] detalhes renegade

--- pontos.py
+++ pontos.py
@@ -1,7 +1,7 @@
 import re
 
 # String do path extraído do SVG
-path_data = "M16.5 89H20L29.5 64.5L61.5 65.5L74 92.5L184.5 93.5L194 65.5H232.5L240.5 92H248.5L256.5 86.5V83.5L265 71V67L263.5 65V59.5L259 56L257.5 55V47L256 44L246 37.5L227.5 33L192 29.5L161 4L125.5 1.5L74 0.5L28 3L17 4.5L13.5 4L12 6L16.5 9L9 28.5L7 31L6 35V47H5V52L3.5 63.5L1 65.5L2 77.5L10.5 87L16.5 89Z"
+path_data = "M194.5 34.5L192.5 29.5L161 4L162.5 7.5L185 29L190 32.5L194.5 34.5Z"
 
 
 # Lista para armazenar os pontos
@@ -46,4 +46,4 @@ while i < len(commands):
 
 # Exibir os pontos
 for point in points:
-    print(f"applyRampTransform(Point(this->start.getX() + {int(point[0])}, yAssoalho + {int(point[1])}), this->degrees),")
+    print(f"applyRampTransform(Point(xStart + {int(point[0])}, yStart + {int(point[1])}), this->degrees),")
--- src/Renegade.cpp
+++ src/Renegade.cpp
@@ -65,55 +65,165 @@ void Renegade::drawPolygon(std::vector<Point> points, Color cor) {
 
 
 void Renegade::draw() {
-    int yAssoalho = this->start.getY() - 100;
+    int xStart = this->start.getX();
+    int yStart = this->start.getY() - 100;
 
-    std::vector<Point> carBody = {
-        applyRampTransform(Point(this->start.getX() + 16, yAssoalho + 89), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 20, yAssoalho + 89), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 29, yAssoalho + 64), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 61, yAssoalho + 65), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 74, yAssoalho + 92), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 184, yAssoalho + 93), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 194, yAssoalho + 65), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 232, yAssoalho + 65), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 240, yAssoalho + 92), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 248, yAssoalho + 92), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 256, yAssoalho + 86), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 256, yAssoalho + 83), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 265, yAssoalho + 71), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 265, yAssoalho + 67), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 263, yAssoalho + 65), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 263, yAssoalho + 59), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 259, yAssoalho + 56), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 257, yAssoalho + 55), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 257, yAssoalho + 47), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 256, yAssoalho + 44), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 246, yAssoalho + 37), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 227, yAssoalho + 33), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 192, yAssoalho + 29), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 161, yAssoalho + 4), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 125, yAssoalho + 1), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 74, yAssoalho + 0), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 28, yAssoalho + 3), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 17, yAssoalho + 4), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 13, yAssoalho + 4), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 12, yAssoalho + 6), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 16, yAssoalho + 9), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 9, yAssoalho + 28), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 7, yAssoalho + 31), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 6, yAssoalho + 35), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 6, yAssoalho + 47), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 5, yAssoalho + 47), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 5, yAssoalho + 52), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 3, yAssoalho + 63), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 1, yAssoalho + 65), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 2, yAssoalho + 77), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 10, yAssoalho + 87), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 16, yAssoalho + 89), this->degrees),
-        applyRampTransform(Point(this->start.getX() + 16, yAssoalho + 89), this->degrees)
+    std::vector<Point> tailLight = {
+        applyRampTransform(Point(xStart + 8, yStart + 48), this->degrees),
+        applyRampTransform(Point(xStart + 6, yStart + 47), this->degrees),
+        applyRampTransform(Point(xStart + 6, yStart + 34), this->degrees),
+        applyRampTransform(Point(xStart + 11, yStart + 34), this->degrees),
+        applyRampTransform(Point(xStart + 15, yStart + 35), this->degrees),
+        applyRampTransform(Point(xStart + 15, yStart + 41), this->degrees),
+        applyRampTransform(Point(xStart + 9, yStart + 48), this->degrees),
+        applyRampTransform(Point(xStart + 8, yStart + 48), this->degrees),
+        applyRampTransform(Point(xStart + 8, yStart + 48), this->degrees)
     };
 
-    drawPolygon(carBody, this->color);
+    std::vector<Point> carHandle1 = {
+        applyRampTransform(Point(xStart + 133, yStart + 44), this->degrees),
+        applyRampTransform(Point(xStart + 118, yStart + 44), this->degrees),
+        applyRampTransform(Point(xStart + 116, yStart + 46), this->degrees),
+        applyRampTransform(Point(xStart + 118, yStart + 48), this->degrees),
+        applyRampTransform(Point(xStart + 133, yStart + 48), this->degrees),
+        applyRampTransform(Point(xStart + 135, yStart + 47), this->degrees),
+        applyRampTransform(Point(xStart + 133, yStart + 44), this->degrees),
+        applyRampTransform(Point(xStart + 133, yStart + 44), this->degrees)
+    };
+
+    std::vector<Point> carHandle2 = {
+        applyRampTransform(Point(xStart + 74, yStart + 44), this->degrees),
+        applyRampTransform(Point(xStart + 59, yStart + 44), this->degrees),
+        applyRampTransform(Point(xStart + 57, yStart + 46), this->degrees),
+        applyRampTransform(Point(xStart + 59, yStart + 47), this->degrees),
+        applyRampTransform(Point(xStart + 74, yStart + 47), this->degrees),
+        applyRampTransform(Point(xStart + 75, yStart + 46), this->degrees),
+        applyRampTransform(Point(xStart + 74, yStart + 44), this->degrees),
+        applyRampTransform(Point(xStart + 74, yStart + 44), this->degrees)
+    };
+
+    std::vector<Point> window = {
+        applyRampTransform(Point(xStart + 179, yStart + 34), this->degrees),
+        applyRampTransform(Point(xStart + 158, yStart + 9), this->degrees),
+        applyRampTransform(Point(xStart + 147, yStart + 7), this->degrees),
+        applyRampTransform(Point(xStart + 107, yStart + 5), this->degrees),
+        applyRampTransform(Point(xStart + 65, yStart + 5), this->degrees),
+        applyRampTransform(Point(xStart + 48, yStart + 6), this->degrees),
+        applyRampTransform(Point(xStart + 42, yStart + 7), this->degrees),
+        applyRampTransform(Point(xStart + 42, yStart + 23), this->degrees),
+        applyRampTransform(Point(xStart + 44, yStart + 26), this->degrees),
+        applyRampTransform(Point(xStart + 75, yStart + 34), this->degrees),
+        applyRampTransform(Point(xStart + 112, yStart + 36), this->degrees),
+        applyRampTransform(Point(xStart + 169, yStart + 37), this->degrees),
+        applyRampTransform(Point(xStart + 179, yStart + 34), this->degrees),
+        applyRampTransform(Point(xStart + 179, yStart + 34), this->degrees)
+    };
+
+    std::vector<Point> frontWindow = {
+        applyRampTransform(Point(xStart + 194, yStart + 34), this->degrees),
+        applyRampTransform(Point(xStart + 192, yStart + 29), this->degrees),
+        applyRampTransform(Point(xStart + 161, yStart + 4), this->degrees),
+        applyRampTransform(Point(xStart + 162, yStart + 7), this->degrees),
+        applyRampTransform(Point(xStart + 185, yStart + 29), this->degrees),
+        applyRampTransform(Point(xStart + 190, yStart + 32), this->degrees),
+        applyRampTransform(Point(xStart + 194, yStart + 34), this->degrees),
+        applyRampTransform(Point(xStart + 194, yStart + 34), this->degrees)
+    };
+
+    std::vector<Point> door1 = {
+        applyRampTransform(Point(xStart + 57, yStart + 29), this->degrees),
+        applyRampTransform(Point(xStart + 54, yStart + 34), this->degrees),
+        applyRampTransform(Point(xStart + 52, yStart + 39), this->degrees),
+        applyRampTransform(Point(xStart + 52, yStart + 65), this->degrees),
+        applyRampTransform(Point(xStart + 52, yStart + 65), this->degrees),
+        applyRampTransform(Point(xStart + 52, yStart + 39), this->degrees),
+        applyRampTransform(Point(xStart + 54, yStart + 34), this->degrees),
+        applyRampTransform(Point(xStart + 57, yStart + 29), this->degrees)
+    };
+
+    std::vector<Point> door2 = {
+        applyRampTransform(Point(xStart + 107, yStart + 5), this->degrees),
+        applyRampTransform(Point(xStart + 112, yStart + 35), this->degrees),
+        applyRampTransform(Point(xStart + 113, yStart + 46), this->degrees),
+        applyRampTransform(Point(xStart + 118, yStart + 93), this->degrees),
+        applyRampTransform(Point(xStart + 118, yStart + 93), this->degrees),
+        applyRampTransform(Point(xStart + 113, yStart + 46), this->degrees),
+        applyRampTransform(Point(xStart + 112, yStart + 35), this->degrees),
+        applyRampTransform(Point(xStart + 107, yStart + 5), this->degrees)
+    };
+
+    std::vector<Point> door3 = {
+        applyRampTransform(Point(xStart + 179, yStart + 34), this->degrees),
+        applyRampTransform(Point(xStart + 183, yStart + 41), this->degrees),
+        applyRampTransform(Point(xStart + 185, yStart + 54), this->degrees),
+        applyRampTransform(Point(xStart + 185, yStart + 66), this->degrees),
+        applyRampTransform(Point(xStart + 181, yStart + 82), this->degrees),
+        applyRampTransform(Point(xStart + 177, yStart + 93), this->degrees),
+        applyRampTransform(Point(xStart + 177, yStart + 93), this->degrees),
+        applyRampTransform(Point(xStart + 181, yStart + 82), this->degrees),
+        applyRampTransform(Point(xStart + 185, yStart + 66), this->degrees),
+        applyRampTransform(Point(xStart + 185, yStart + 54), this->degrees),
+        applyRampTransform(Point(xStart + 183, yStart + 41), this->degrees),
+        applyRampTransform(Point(xStart + 179, yStart + 34), this->degrees)
+    };
+    
+    std::vector<Point> headLight = {
+        applyRampTransform(Point(xStart + 254, yStart + 45), this->degrees),
+        applyRampTransform(Point(xStart + 256, yStart + 44), this->degrees),
+        applyRampTransform(Point(xStart + 257, yStart + 47), this->degrees),
+        applyRampTransform(Point(xStart + 257, yStart + 55), this->degrees),
+        applyRampTransform(Point(xStart + 257, yStart + 57), this->degrees),
+        applyRampTransform(Point(xStart + 256, yStart + 57), this->degrees),
+        applyRampTransform(Point(xStart + 254, yStart + 55), this->degrees),
+        applyRampTransform(Point(xStart + 254, yStart + 45), this->degrees),
+        applyRampTransform(Point(xStart + 254, yStart + 45), this->degrees)
+    };
+
+    std::vector<Point> carBody = {
+        applyRampTransform(Point(this->start.getX() + 16, yStart + 89), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 20, yStart + 89), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 29, yStart + 64), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 61, yStart + 65), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 74, yStart + 92), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 184, yStart + 93), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 194, yStart + 65), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 232, yStart + 65), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 240, yStart + 92), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 248, yStart + 92), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 256, yStart + 86), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 256, yStart + 83), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 265, yStart + 71), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 265, yStart + 67), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 263, yStart + 65), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 263, yStart + 59), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 259, yStart + 56), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 257, yStart + 55), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 257, yStart + 47), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 256, yStart + 44), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 246, yStart + 37), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 227, yStart + 33), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 192, yStart + 29), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 161, yStart + 4), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 125, yStart + 1), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 74, yStart + 0), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 28, yStart + 3), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 17, yStart + 4), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 13, yStart + 4), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 12, yStart + 6), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 16, yStart + 9), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 9, yStart + 28), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 7, yStart + 31), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 6, yStart + 35), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 6, yStart + 47), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 5, yStart + 47), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 5, yStart + 52), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 3, yStart + 63), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 1, yStart + 65), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 2, yStart + 77), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 10, yStart + 87), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 16, yStart + 89), this->degrees),
+        applyRampTransform(Point(this->start.getX() + 16, yStart + 89), this->degrees)
+    };
 
     Point pCenterWheel1 = applyRampTransform(Point(this->start.getX() + 46, this->start.getY() - 10), this->degrees);
     Wheel wheel1 = Wheel(pCenterWheel1, 22, this->wheelRotation, this->color); // Roda traseira
@@ -123,4 +233,15 @@ void Renegade::draw() {
 
     wheel1.draw();
     wheel2.draw();
+    
+    drawPolygon(tailLight, this->color);
+    drawPolygon(carHandle1, this->color);
+    drawPolygon(carHandle2, this->color);
+    drawPolygon(window, this->color);
+    drawPolygon(frontWindow, this->color);
+    drawPolygon(door1, this->color);
+    drawPolygon(door2, this->color);
+    drawPolygon(door3, this->color);
+    drawPolygon(headLight, this->color);
+    drawPolygon(carBody, this->color);
 }
